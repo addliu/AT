5e196a60a70d656fa01b6feef94b1f64ce5fa2b2
addliu <pyliuchuang@gmail.com>
2016-09-22 23:39:09 +0800

    first commit

---
+++ TreeNode.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# 计算二叉树的深度
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def tree_depth(self, pRoot):
+        # write code here
+        if pRoot is None:
+            return 0
+        return max(1 + self.tree_depth(pRoot.left), 1 + self.tree_depth(pRoot.right))
+
+
+t = TreeNode(1)
+t.left = TreeNode(2)
+t.left.left = TreeNode(2)
+t.left.right = TreeNode(2)
+t.right = TreeNode(2)
+t.right.right = TreeNode(2)
+t.right.right.right = TreeNode(2)
+
+print(Solution().tree_depth(t))
+
+p = TreeNode(1)
+print(Solution().tree_depth(p))
