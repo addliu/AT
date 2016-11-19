524c0e0adfa8cb8a5855e67898a220de807b5305
addliu <pyliuchuang@gmail.com>
2016-11-19 23:22:45 +0800

    make test code run only if namespace is this module

--- TreeNode.py
+++ TreeNode.py
@@ -17,16 +17,16 @@ class Solution:
             return 0
         return max(1 + self.tree_depth(pRoot.left), 1 + self.tree_depth(pRoot.right))
 
+if __name__ == '__main__':
+    t = TreeNode(1)
+    t.left = TreeNode(2)
+    t.left.left = TreeNode(2)
+    t.left.right = TreeNode(2)
+    t.right = TreeNode(2)
+    t.right.right = TreeNode(2)
+    t.right.right.right = TreeNode(2)
 
-t = TreeNode(1)
-t.left = TreeNode(2)
-t.left.left = TreeNode(2)
-t.left.right = TreeNode(2)
-t.right = TreeNode(2)
-t.right.right = TreeNode(2)
-t.right.right.right = TreeNode(2)
+    print(Solution().tree_depth(t))
 
-print(Solution().tree_depth(t))
-
-p = TreeNode(1)
-print(Solution().tree_depth(p))
+    p = TreeNode(1)
+    print(Solution().tree_depth(p))
